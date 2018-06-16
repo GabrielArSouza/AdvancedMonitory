0a425321c381ec2326aaf45de495d71c9b3229c5
GabrielArSouza <gabriel_feg@hotmail.com>
2018-06-16 16:31:10 -0300

    add guard condition from application

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+
+int main ()
+{
+	char guard = 'c';
+
+	std::cout << "Running application\n";
+	std::cout << "Press 'q' to exit\n";
+	
+	while ( (guard = getchar()) != 'q')
+	{
+		
+	}
+
+	return 0;
+}
